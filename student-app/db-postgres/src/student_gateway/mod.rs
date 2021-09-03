9d8ff8768799674e8d28c577cf5a03c1951e655a
nhuthm080280 <nhuthm080280@gmail.com>
2021-09-03 10:40:40 +0700

    trigger build

--- student-app/db-postgres/src/student_gateway/mod.rs
+++ student-app/db-postgres/src/student_gateway/mod.rs
@@ -1,6 +1,2 @@
-use async_trait::async_trait;
-use tokio_postgres::{Client, Error, Row};
-use uuid::Uuid;
-
 mod mutation;
 mod query;
--- student-app/db-postgres/src/student_gateway/mutation.rs
+++ student-app/db-postgres/src/student_gateway/mutation.rs
@@ -1,3 +1,1 @@
-use tokio_postgres::types::ToSql;
-use tokio_postgres::{Client, Error, Row, Transaction};
-use uuid::Uuid;
+
