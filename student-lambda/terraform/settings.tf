2ec2855e8adbf8604ae987f71b2ee6ce1d230d0a
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-12-11 16:01:03 +0000

    Clean up Terraform

--- student-lambda/terraform/settings.tf
+++ student-lambda/terraform/settings.tf
@@ -1,3 +1,28 @@
+module "student-service" {
+  source       = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
+  service_name = var.service_name
+  path_part    = var.service_name
+  file_name    = var.service_name
+  environment  = var.environment
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    tfe = {
+      version = "~> 0.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
 variable "service_name" {}
 
 variable "organisation" {
