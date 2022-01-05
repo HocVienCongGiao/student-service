00255a902d1d7e8720a92d71769c31d1ba81bb49
pmhieu <minhhieu060799@gmail.com>
2022-01-05 20:55:56 +0700

    [UPDATE] entities

--- student-app/domain/src/entities/student.rs
+++ student-app/domain/src/entities/student.rs
@@ -1,17 +1,7 @@
-use chrono::{DateTime, NaiveDate, Utc};
 use uuid::Uuid;
 
 pub(crate) struct Student {
-    pub id: Option<Uuid>,
     pub person_id: Option<Uuid>,
-    pub date_of_birth: Option<NaiveDate>,
-    pub place_of_birth: Option<String>,
-    pub email: Option<String>,
-    pub phone: Option<String>,
-    pub educational_stage: Option<Vec<EducationalStage>>,
-    pub nationality: Option<String>,
-    pub race: Option<String>,
-    pub address: Option<String>,
 }
 
 impl Student {
